0bbd014fed41d1bf7d6a9ffac28e17bd40761e2e
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2021-02-10 18:19:56 +0800

    Buttons are expanded in their columns and the columns are stretched along its crossaxis

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,68 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Container(
             // ignore: deprecated_member_use
             child: Column(
-              children: [
-                FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playerFunction(1);
-                  },
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      playerFunction(1);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playerFunction(2);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.orange,
+                    onPressed: () {
+                      playerFunction(2);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playerFunction(3);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.yellow,
+                    onPressed: () {
+                      playerFunction(3);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playerFunction(4);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      playerFunction(4);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    playerFunction(5);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      playerFunction(5);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.indigo,
-                  onPressed: () {
-                    playerFunction(6);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.indigo,
+                    onPressed: () {
+                      playerFunction(6);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    playerFunction(7);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.purple,
+                    onPressed: () {
+                      playerFunction(7);
+                    },
+                  ),
                 ),
               ],
             ),
