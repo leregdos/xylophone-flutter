7f3a72b7786f3053aac76c48f2d318360029e20c
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2021-02-10 15:45:15 +0800

    Implemented buttons and built a function to play sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playerFunction(int n) {
+    final player = AudioCache();
+    player.play('note$n.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            // ignore: deprecated_member_use
+            child: Column(
+              children: [
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playerFunction(1);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playerFunction(2);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playerFunction(3);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playerFunction(4);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playerFunction(5);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.indigo,
+                  onPressed: () {
+                    playerFunction(6);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playerFunction(7);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
